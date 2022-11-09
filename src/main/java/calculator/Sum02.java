ae1c25789a565dadcb17dbbefd459247374547a3
Sunnyjam <gustjs0540@naver.com>
2022-11-09 17:01:26 +0900

    숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환

---
+++ src/main/java/calculator/Sum02.java
@@ -0,0 +1,13 @@
+package main.java.calculator;
+
+public class Sum02 {
+    //숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환
+    public static int sum ( int c ){
+        return c;
+    }
+    public static void main(String[] args) {
+
+        String number = "7,11";
+        System.out.println(number.substring(0,4));
+    }
+}
