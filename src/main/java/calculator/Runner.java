8514025e4ddd89c7905dd0701db74437833ab92a
flab-blackship <hyunhu0309@gmail.com>
2022-11-12 00:07:04 +0900

    프로젝트 구조와 코드 리팩토링 버전 - 요구사항 3

--- src/main/java/calculator/Runner.java
+++ src/main/java/calculator/Runner.java
@@ -6,7 +6,7 @@ package main.java.calculator;
 public class Runner {
     public static void main(String[] args) {
         StringCalculator sc = new StringCalculator();
-        int answer = sc.calculate("1,2");
+        int answer = sc.calculate("-1:2,3");
         System.out.println("ANSWER : " + answer);
     }
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,7 +2,7 @@ package main.java.calculator;
 
 public class StringCalculator {
     private static final int ZERO = 0;
-    private static final String SEPARATOR_COMMA = ",";
+    private static final String DEFAULT_SEPARATOR = "[,:]";
 
     public int calculate(String inputString) {
         /** 빈 문자열 또는 null 값 입력시 0 반환*/
@@ -10,11 +10,12 @@ public class StringCalculator {
             return ZERO;
         }
         /** 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환 */
-        if (!inputString.contains(",")) {
+        /** 구분자를 컴마(,) 이외에 콜론(:)을 사용 가능 */
+        if (!inputString.contains(",") && !inputString.contains(":")) {
             return Integer.parseInt(inputString);
         }
         int calculations = ZERO;
-        for (String element : inputString.split(SEPARATOR_COMMA)) {
+        for (String element : inputString.split(DEFAULT_SEPARATOR)) {
             calculations += Integer.parseInt(element);
         }
 
