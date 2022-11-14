45a6d5c8a701e9cbe01cc167878331f61bdbdc5e
halfdev <gusgodnd@naver.com>
2022-11-14 13:02:12 +0900

    “//”와 “\n” 문자 사이에 커스텀 구분자를 지정 가능 (Refactoring도 추가함)

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,24 +1,41 @@
 package main.java.calculator;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     private static final int ZERO = 0;
-    private static final String DEFAULT_SEPARATOR = "[,:]";
+    private static final String DEFAULT_SEPARATOR = "[,:]"; // 요구사항 2,3
+    private static final String CUSTOM_SEPARATOR = "//(.)\n(.*)"; // “//”와 “\n” 문자 사이에 커스텀 구분자를 지정 가능
 
     public int calculate(String inputString) {
-        /** 빈 문자열 또는 null 값 입력시 0 반환*/
-        if (inputString == null || "".equals(inputString)) {
+
+        if (checkIfEmpty(inputString)) {
             return ZERO;
         }
-        /** 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환 */
-        /** 구분자를 컴마(,) 이외에 콜론(:)을 사용 가능 */
-        if (!inputString.contains(",") && !inputString.contains(":")) {
-            return Integer.parseInt(inputString);
-        }
-        int calculations = ZERO;
-        for (String element : inputString.split(DEFAULT_SEPARATOR)) {
-            calculations += Integer.parseInt(element);
+
+        Matcher m = Pattern.compile(CUSTOM_SEPARATOR).matcher(inputString);
+        if (m.find()) {
+            String customSeparator = m.group(1);
+            String[] data = m.group(2).split(customSeparator);
+
+            return Arrays
+                    .stream(data)
+                    .map(Integer::parseInt) //숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
+                    .reduce((i1, i2) -> i1 + i2)
+                    .get();
         }
 
-        return calculations;
+        return Arrays
+                .stream(inputString.split(DEFAULT_SEPARATOR))
+                .map(Integer::parseInt) //숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
+                .reduce((i1, i2) -> i1 + i2)
+                .get();
+    }
+
+    //빈 문자열 또는 null 값을 입력할 경우 0을 반환
+    private boolean checkIfEmpty(String inputString) {
+        return inputString == null || "".equals(inputString);
     }
 }
