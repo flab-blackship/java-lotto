5af110dcc6013e35ab224630d25ee6c4308c5927
Sunnyjam <gustjs0540@naver.com>
2022-11-17 21:06:25 +0900

    음수를 전달할 경우 RuntimeException 예외가 발생

--- src/main/java/calculator/Exception04.java
+++ src/main/java/calculator/Exception04.java
@@ -3,19 +3,12 @@ package main.java.calculator;
 public class Exception04 {
     //음수를 전달할 경우 RuntimeException 예외가 발생
 
-    public int exception( int minus){
-
-        int reset = 1;
-
-        try {
-            for ( int i =0; i >= 0; i++){
-                //여기에 어떤게 들어가야하는 지 모르겠어요..ㅜㅜ
-            }
-        }catch ( RuntimeException e){
-            System.out.println("음수를 전달하였습니다.");
+    public int exception (int num){
+        if ( num > 0) {
+            return num;
+        } else {
+            throw new RuntimeException("음수 입니다.");
         }
-        return reset;
     }
 
 }
-// public int exception에서 exception가 Runner클래스에서 작용이 안되요..
--- src/main/java/calculator/Runner.java
+++ src/main/java/calculator/Runner.java
@@ -2,8 +2,11 @@ package main.java.calculator;
 
 public class Runner {
     public static void main(String[] args) {
-        Sum04 plus = new Sum04();
-        System.out.println(plus.sum("11","1"));
-        System.out.println(plus.);
+//        Sum04 plus = new Sum04();
+//        System.out.println(plus.sum("11","1"));
+
+        Exception04 act = new Exception04();
+        System.out.println(act.exception(3));
+
     }
 }
