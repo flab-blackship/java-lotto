3fffa1299c8b5223c5f5beeff7366acb0b3236b8
blackship <114370559+flab-blackship@users.noreply.github.com>
2022-11-25 08:42:19 +0900

    Create LottoTicket.java

---
+++ src/main/java/lotto/LottoTicket.java
@@ -0,0 +1,38 @@
+package main.java.lotto;
+
+public class LottoTicket {
+
+    static final int BASIC_LOTTO_SIZE = 6;
+    private final List<Integer> lottoTicket;
+
+    private LottoTicket(List<Integer> lottoTicket) {
+        this.lottoTicket = lottoTicket;
+    }
+
+    public static LottoTicket issue() {
+        return new LottoTicket(pickLottoBalls(generateLottoBalls()));
+    }
+
+    static List<Integer> pickLottoBalls(List<Integer> lottoBalls) {
+        shuffleLottoBalls(lottoBalls);
+        return lottoBalls.stream()
+                .limit(BASIC_LOTTO_SIZE)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    static void shuffleLottoBalls(List<Integer> lottoBalls) {
+        Collections.shuffle(lottoBalls);
+    }
+
+    static List<Integer> generateLottoBalls() {
+        return IntStream
+                .rangeClosed(LottoNumber.MINIMUM_LOTTO_NUMBER, LottoNumber.MAXIMUM_LOTTO_NUMBER)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getLottoTicket() {
+        return Collections.unmodifiableList(lottoTicket);
+    }
+}
